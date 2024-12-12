72343ae1a4fd15ab2900453ce24bada18c951d7a
hunko <rhwlgns4386@naver.com>
2024-12-12 16:37:22 +0900

    스크립트 수정2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,14 +4,16 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션pid 확인"
-IDEL_PID=$(lsof -ti tcp:${IDLE_PORT})
+IDLE_PORT=$(find_idle_port)
 
-if [ -z ${IDEL_PID} ]
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 IDEL_PID"
-    kill -15 ${IDEL_PID}
-    sleep 5
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
 fi
